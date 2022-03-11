9ed03a26254976e791f0863439be534aa32b3983
Antakathena <s.trebatius@gmail.com>
2022-03-11 15:55:50 +0100

    se rapprocher des wireframes

--- src/reviews/models.py
+++ src/reviews/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 
 
 class Ticket(models.Model):
@@ -11,9 +12,15 @@ class Ticket(models.Model):
     image = models.ImageField(null=True, blank=True)
     time_created = models.DateTimeField(auto_now_add=True)
 
-
     def __str__(self):
         return f"Demande à propos de \"{self.title}\" par {self.user}"
+
+    def get_absolute_url(self):
+        return reverse('reviews-ticket-detail', kwargs={'pk': self.pk})
+
+    # redirect renvoie à une adresse specifique
+    # reverse va seulement donner l'adresse en string à la vue
+
 
 class Review(models.Model):
     ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
